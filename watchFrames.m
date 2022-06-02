a8b6f6c5916290c109c37e90ca7864d252a45108
Ledjo <ledjo.lleshaj@gmail.com>
2022-06-02 15:08:45 -0700

    Displays video frames

---
+++ watchFrames.m
@@ -0,0 +1,22 @@
+    % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+    %   watchVideo Mostra la video in frames                        %
+    %                                                               %
+    %  INPUT                                                        %
+    %   filename: Nome del file del video di frame;                 %
+    %  OUTPUT                                                       %
+    %   void                                                        %
+    % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+
+function watchFrames(filename)
+    
+% Carico il video di frame con il nome dato
+load(filename, 'frames');
+    
+% Scorro tutto il video e mostro ogni frame con il relativo indice
+figure;
+for i=1:size(frames, 4)
+    imshow(frames(:,:,:,i)); title(strcat('Frame number: ', num2str(i)));
+    pause(0.1);
+end
+    
+end
