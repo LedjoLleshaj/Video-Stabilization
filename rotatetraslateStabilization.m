dcf88639317953a32541e9edde9c113b3e922430
Ledjo <ledjo.lleshaj@gmail.com>
2022-06-04 07:06:07 -0700

    Fixed bugs

---
+++ rotatetraslateStabilization.m
@@ -0,0 +1,45 @@
+function rotatetraslateStabilization(filename) 
+    % Reset the video source to the beginning of the file.
+    %filename = 'shaky_car.avi';
+    hVideoSrc = VideoReader(filename);
+    
+    % Prendiamo l'ancora dall'immagine
+    figure; 
+    anchor = imcrop(rgb2gray(im2single(readFrame(hVideoSrc))););
+    close;
+    [R,C,~] = size(anchor);
+    %Check risoluzione
+    res = R*C; 
+    tipo = 3;% res > 1280*720
+    if( res < 640*480) 
+        tipo = 1; 
+    end
+    if( res >= 640*480 && res < 1280*720) 
+        tipo = 2; 
+    end    
+        
+    display(tipo);
+        
+    R = round(R/2);
+    C = round(C/2);
+    %Inizio stabilizzazione dei frame
+    
+    ang = 0;
+    for i = 1:hVideoSrc.NumFrames-1        
+        frame = hVideoSrc.readFrame();
+        [offset,ang] = CrossCorr(R,C,anchor,rgb2gray(frame),ang,tipo);
+        stabilizedFrame = imtranslate(imrotate(frame,ang,'bilinear','crop'),offset,'FillValues',0);
+        
+        videoOutput(:,:,:,i) = stabilizedFrame; %add stabilized frame to video output  
+    end
+
+    close(f);
+    
+    % Write stabilized video
+    output = VideoWriter(strcat(filename,'_rtout'));
+    open(output);
+    for i=1:nFrames-1
+        writeVideo(output,mat2gray(videoOutput(:,:,:,i)));
+    end
+    close(output);
+ end
